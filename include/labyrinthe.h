5a3a50b142000e8a26ee91fdeb2386c6d47407e4
Amiard Anthony <s176975@ic2s122-04.univ-lemans.fr>
2019-02-28 11:09:25 +0100

    Creation de labyrinthe.h

---
+++ include/labyrinthe.h
@@ -0,0 +1,44 @@
+/*
+ * Definition des constantes et des fonctions permettant de generer un
+ * labyrinthe
+ */
+
+#ifndef _LABYRINTHE_H_
+#define _LABYRINTHE_H_
+
+
+
+/*
+ * CONSTANTES
+ */
+
+
+#ifndef N_LAB
+#define N_LAB 28
+#endif
+
+#ifndef M_LAB
+#define M_LAB 31
+#endif
+
+
+
+/*
+ * FONCTIONS
+ */
+ 
+/* Genere un labyrinthe aleatoire dans le tableau labyrinthe
+ * 'm' = mur
+ * 'c' = chemin vide
+ * 'p' = chemin avec pacgum
+ * 's' = chemin avec super pacgum
+ * 'b' = boite des fantomes
+ * 'e' = entree de la boite
+ * Ecrit le nombre de pacgum presents dans le labyrinthe dans nb_pacgums
+ * Renvoie 0 si tout se passe bien
+ */
+int genere_lab(char labyrinthe[N_LAB][M_LAB], int * nb_pacgums);
+
+
+
+#endif
