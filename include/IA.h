532afc45e2d4990fd26d70f11c04f7d2afe29eaf
Ambroise Taliesin <s173833@ic2s122-03.univ-lemans.fr>
2019-02-28 11:49:25 +0100

    Ajout du include objets.h dans ia.h

--- include/IA.h
+++ include/IA.h
@@ -5,6 +5,7 @@
 #ifndef _IA_H_
 #define _IA_H_
 #include "taille_lab.h"
+#include "objets.h"
 
 /* Fonction trouver chemin le plus court pour atteindre le joueur */
 coord_t chemin_court(char labyrinthe[N_LAB][M_LAB], coord_t * coord_dep, coord_t * coord_arr);
