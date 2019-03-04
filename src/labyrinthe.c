d778e72691525fd438d729b7b74e9161c9cb871e
Anthony <anthony@3615yu.noho.st>
2019-03-04 20:10:58 +0100

    Premiere approche de la generation de labyrinthe

--- src/labyrinthe.c
+++ src/labyrinthe.c
@@ -7,6 +7,8 @@
 
 #include "../include/labyrinthe.h"
 
+#define N_RAND 2
+
 void aff_lab(const char labyrinthe[N_LAB][M_LAB]) {
 	int i, j;
 	for(i = 0; i < N_LAB; i++) {
@@ -23,6 +25,104 @@ void aff_lab(const char labyrinthe[N_LAB][M_LAB]) {
 		}
 		printf("\n");
 	}
+}
+
+int nb_voisin(char labyrinthe[N_LAB][M_LAB / 2], int x, int y) {
+	int nb = 0;
+	if(x > 0 && (labyrinthe[y][x-1] == 'c' || labyrinthe[y][x-1] == 'p' ||
+				 labyrinthe[y][x-1] == 's')) {
+		nb++;
+	}
+	if(x < M_LAB / 2 - 1 && (labyrinthe[y][x+1] == 'c' ||
+	   labyrinthe[y][x+1] == 'p' || labyrinthe[y][x+1] == 's')) {
+		nb++;
+	}
+	if(y > 0 && (labyrinthe[y-1][x] == 'c' || labyrinthe[y-1][x] == 'p' ||
+				 labyrinthe[y-1][x] == 's')) {
+		nb++;
+	}
+	if(y < M_LAB / 2 - 1 && (labyrinthe[y+1][x] == 'c' ||
+	   labyrinthe[y+1][x] == 'p' || labyrinthe[y+1][x] == 's')) {
+		nb++;
+	}
+	return nb;
+}
+
+void chemin_alea(char labyrinthe[N_LAB][M_LAB / 2], int x, int y) {
+	int mid = M_LAB / 2;
+	if(labyrinthe[y][x] == 'm') {
+		labyrinthe[y][x] = 'p';
+		if(x < mid - 1 && (labyrinthe[y][x-1] == 'm' || labyrinthe[y][x+1] == 'm' ||
+		labyrinthe[y-1][x] == 'm' || labyrinthe[y+1][x] == 'm')) {
+			if(x - 1 > 0) {
+				if(rand() % N_RAND)
+					chemin_alea(labyrinthe, x - 1, y);
+			}
+			if(y - 1 > 0) {
+				if(rand() % N_RAND)
+					chemin_alea(labyrinthe, x, y - 1);
+			}
+			if(y + 1 < N_LAB - 1) {
+				if(rand() % N_RAND)
+					chemin_alea(labyrinthe, x, y + 1);
+			}
+			if(rand() % N_RAND)
+				chemin_alea(labyrinthe, x + 1, y);
+		}
+	}
+}
+
+int genere_lab(char labyrinthe[N_LAB][M_LAB], int * nb_pacgums) {
+	int mid = M_LAB / 2;
+	char base[N_LAB][M_LAB / 2] = {
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c', 'c', 'c', 'c', 'c'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c', 'b', 'b', 'b', 'e'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c', 'b', 'b', 'b', 'b'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c', 'b', 'b', 'b', 'b'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c', 'b', 'b', 'b', 'b'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c', 'b', 'b', 'b', 'b'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c', 'c', 'c', 'c', 'c'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'c'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'},
+		{'m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'}
+	};
+	
+	*nb_pacgums = 0;
+	
+	chemin_alea(base, 1, 1);
+	
+	int i, j;
+	for(i = 0; i < N_LAB; i++) {
+		for(j = 0; j < mid; j++) {
+			labyrinthe[i][j] = base[i][j];
+			labyrinthe[i][M_LAB - 1 - j] = base[i][j];
+			if(base[i][j] == 'p')
+				*nb_pacgums += 2;
+		}
+	}
+	
+	return 0;
 }
 
 int lab_manuel(char labyrinthe[N_LAB][M_LAB], int * nb_pacgums) {
--- test/test_lab.c
+++ test/test_lab.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "../include/labyrinthe.h"
 
 int main() {
+	srand(time(NULL));
+	
 	char labyrinthe[N_LAB][M_LAB];
 	int nb_pacgums;
 	lab_manuel(labyrinthe, &nb_pacgums);
+	aff_lab(labyrinthe);
 	
+	printf("\n");
+	
+	genere_lab(labyrinthe, &nb_pacgums);
 	aff_lab(labyrinthe);
 	
 	return 0;
