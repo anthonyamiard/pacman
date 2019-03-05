6d90bb137bca6c61e6d42d718aa4153116ae3c0d
Bigan Hugo <s183174@ic2s122-03.univ-lemans.fr>
2019-03-05 16:23:35 +0100

    Modification IA.c

--- src/IA.c
+++ src/IA.c
@@ -10,25 +10,40 @@
 #include "../include/labyrinthe.h"
 
 
-/*Pathfinding pour M cases */
-
 
 coord_t chemin_court(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-	fantome_t * f_bleu;
+
 }
 
 coord_t chemin_anticipe(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-	fantome_t * f_rose;
+
 }
 
 coord_t chemin_fuir(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-	fantome_t * f_orange;
+
 }
 
 coord_t chemin_aleatoire(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-	fantome_t * f_rouge;
-	
-}
+
+	int x,y;
+	coord_t res;
+
+	x = coord_dep->x;
+	y = coord_dep->y;
+
+	do{
+		if(rand() %2)
+			res.x=x+1;
+		else
+			res.x=x-1;
+		if(rand() %2)
+			res.y=y+1;
+		else
+			res.y=y-1;
+	}while(!est_chemin(labyrinthe[res.y][res.x]));
+
+	return res;
+}	
 
 int fantome(char labyrinthe[N_LAB][M_LAB], fantome_t* fantome, joueur_t* joueur, int vitesse) {
 
