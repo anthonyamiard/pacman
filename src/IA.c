59b9a1cb00757b940a59d7f26ce8463c5fa3d0ff
Bigan Hugo <s183174@ic2s122-03.univ-lemans.fr>
2019-03-05 16:04:38 +0100

    Modification de test_IA.c

--- src/IA.c
+++ src/IA.c
@@ -9,20 +9,25 @@
 #include "../include/objets.h"
 #include "../include/labyrinthe.h"
 
-coord_t chemin_court(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
 
+/*Pathfinding pour M cases */
+
+
+coord_t chemin_court(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
+	fantome_t * f_bleu;
 }
 
 coord_t chemin_anticipe(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-
+	fantome_t * f_rose;
 }
 
 coord_t chemin_fuir(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-
+	fantome_t * f_orange;
 }
 
 coord_t chemin_aleatoire(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-
+	fantome_t * f_rouge;
+	
 }
 
 int fantome(char labyrinthe[N_LAB][M_LAB], fantome_t* fantome, joueur_t* joueur, int vitesse) {
