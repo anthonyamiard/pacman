fa8ef445260d33aaaf3c6eef9fbde95e768997a8
Bigan Hugo <s183174@ic2s122-03.univ-lemans.fr>
2019-03-05 14:19:24 +0100

    Ajout de test IA

---
+++ test/test_IA.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../include/IA.h"
+#include "../include/labyrinthe.h"
+
+int main() {
+	srand(time(NULL));
+	
+	
+	return 0;
+}
