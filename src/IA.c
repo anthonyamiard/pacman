7780d139bb0239ee85f05caa0a8352766d2c0896
Ambroise Taliesin <s173833@ic2s122-05.univ-lemans.fr>
2019-03-05 15:30:09 +0100

    Ajout de IA.c

---
+++ src/IA.c
@@ -0,0 +1,34 @@
+/*
+ * Fonctions permettant de gerer l'intelligence artificielle
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/IA.h"
+#include "../include/objets.h"
+#include "../include/labyrinthe.h"
+
+coord_t chemin_court(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
+
+}
+
+coord_t chemin_anticipe(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
+
+}
+
+coord_t chemin_fuir(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
+
+}
+
+coord_t chemin_aleatoire(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
+
+}
+
+int fantome(char labyrinthe[N_LAB][M_LAB], fantome_t* fantome, joueur_t* joueur, int vitesse) {
+
+}
+
+int ramasse_pacgum(char labyrinthe[N_LAB][M_LAB], joueur_t* joueur) {
+
+}
