111ae0613ebe8f299aa00dba18f15786cd629c2b
Ambroise Taliesin <s173833@ic2s130-01.univ-lemans.fr>
2019-03-12 14:29:48 +0100

    Modification d'un comentaire, utiliser p au lieu de s pour fantome rose

--- include/objets.h
+++ include/objets.h
@@ -2,7 +2,7 @@
  * Definition des types et des fonctions permettant de manipuler les differents
  * objets
  */
- 
+
 #ifndef _OBJETS_H_
 #define _OBJETS_H_
 
@@ -13,7 +13,7 @@
 /*
  * TYPES
  */
- 
+
 /* Coordonnees */
 typedef struct coord_s {
 	int x, y;
@@ -39,7 +39,7 @@ typedef struct joueur_s {
 /* Fantome */
 typedef struct fantome_s {
 	char * nom;
-	char couleur; /* b = bleu, r = rouge, s = rose, o = orange */
+	char couleur; /* b = bleu, r = rouge, p = rose, o = orange */
 	coord_t (*chemin)(char [N_LAB][M_LAB], coord_t *, coord_t *);
 	etat_t etat;
 	coord_t * coord;
@@ -51,7 +51,7 @@ typedef struct fantome_s {
  * FONCTIONS DE CREATION DES OBJETS
  * Renvoient un pointeur sur les objets
  */
- 
+
 /* Coordonnees */
 coord_t * cree_coord(int x, int y);
 
