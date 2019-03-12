a953d61645645e9a041b3fa6b64853d9eb205fb1
Bigan Hugo <s183174@ic2s130-03.univ-lemans.fr>
2019-03-12 16:27:14 +0100

    Modification ia.c

--- src/IA.c
+++ src/IA.c
@@ -11,31 +11,73 @@
 #include "../include/labyrinthe.h"
 
 #define SCORE_PACGUM 10
+#define FAUX 0
+#define VRAI 1
 
 coord_t chemin_court(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-  	int i,j;
-	int lab_numero[N_LAB][M_LAB];
-  int trouve = 0;
+  int i,j;
+  int trouve = FAUX;
   int nb_courant = 1;
-	temp = coord_arr;
-
-	for(i=0;i<30;i++){
-		for(j=0;j<27;j++){
- 			if(labyrinthe[i][j] == 'm')
-				lab_numero[i][j] = -1;
-			else
-				lab_numero[i][j] == 0;
-		}
-	}
-
+  int lab_numero[N_LAB][M_LAB];
+  lab_numero[coord_dep->y][coord_dep->x] = 1;
+	   for(i=0;i<30 && !trouve;i++){
+		     for(j=0;j<27 && !trouve;j++){
+ 			       if(est_chemin(labyrinthe[i][j])){
+				           lab_numero[i][j] = nb_courant;
+                   if (lab_numero[i][j] == nb_courant){
+                     if(i-1>=0){
+                       if(lab_numero[i-1][j] == 0)
+                          lab_numero[i-1][j] = nb_courant + 1;
+                     }
+                     if(j-1>=0){
+                       if(lab_numero[i][j-1] == 0)
+                          lab_numero[i][j-1] = nb_courant +1;
+                     }
+                     if(i+1>=0){
+                       if(lab_numero[i+1][j] == 0)
+                          lab_numero[i+1][j] = nb_courant +1;
+                     }
+                     if(j+1>=0){
+                       if(lab_numero[i][j+1] == 0)
+                          lab_numero[i][j+1] = nb_courant +1;
+                     }
+                     if(lab_numero[coord_arr->y][coord_arr->x] == nb_courant + 1)
+                        trouve = VRAI;
+                  nb_courant ++;
+                  }
+            }
+	     }
+    }
+ nb_courant = lab_numero[coord_arr->y][coord_arr->x];
+ while(nb_courant > 1){
+   if(i - 1 >= 0 && lab_numero[i-1][j] == nb_courant - 1)
+      i = i - 1;
+   else if(j - 1 >= 0 && lab_numero[i][j-1] == nb_courant - 1)
+      j = j - 1;
+   else if(i + 1 >= 0 && lab_numero[i+1][j] == nb_courant - 1)
+      i = i - 1;
+   else if(j + 1 >= 0 && lab_numero[i][j+1] == nb_courant - 1)
+      j = j - 1;
+   nb_courant --;
+ }
+ coord_t retour;
+ retour.y=i;
+ retour.x=j;
+ return retour;
 }
 
 coord_t chemin_anticipe(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-
+  coord_t retour;
+  retour.y=0;
+  retour.x=0;
+  return retour;
 }
 
 coord_t chemin_fuir(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
-    /*inverse de chemin court?*/
+  coord_t retour;
+  retour.y=0;
+  retour.x=0;
+  return retour;
 }
 
 coord_t chemin_aleatoire(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coord_t* coord_arr) {
@@ -55,7 +97,7 @@ coord_t chemin_aleatoire(char labyrinthe[N_LAB][M_LAB], coord_t* coord_dep, coor
 
 int fantome(char labyrinthe[N_LAB][M_LAB], fantome_t* fantome, joueur_t* joueur, int vitesse) {
   coord_t temp;
-  temp  = fantome->chemin(labyrinthe,fantome->coord,joeur->coord);
+  temp  = fantome->chemin(labyrinthe,fantome->coord,joueur->coord);
   fantome->coord->x=temp.x;
   fantome->coord->y=temp.y;
   return 0;
