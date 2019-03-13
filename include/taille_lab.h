5dc25d32cf1a8f75d6df05a63c34d5863c38eab1
Anthony <anthony@3615yu.noho.st>
2019-03-13 22:36:26 +0100

    Commentaires

--- include/taille_lab.h
+++ include/taille_lab.h
@@ -1,11 +1,16 @@
-/*
+/*!
+ * \file	taille_lab.h
+ * \author	Anthony Amiard
+ * \date	2019
+ * 
  * Constantes de la taille du labyrinthe
+ * 
  */
 
 #ifndef _TAILLE_LAB_H_
 #define _TAILLE_LAB_H_
 
-#define M_LAB 28
-#define N_LAB 31
+#define M_LAB 28 /*!< Nombre de colonnes du labyrinthe */
+#define N_LAB 31 /*!< Nombre de lignes du labyrinthe */
 
 #endif
