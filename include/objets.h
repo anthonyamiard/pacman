250ed813d0df4c2d0de6e573f1864c6bf81ecb55
Anthony <anthony@3615yu.noho.st>
2019-03-19 15:34:53 +0100

    Ajout de commentaires et changement des chaines dynamiques en statique

--- include/objets.h
+++ include/objets.h
@@ -13,6 +13,9 @@
 
 #include "taille_lab.h"
 
+/*! Taille maximale des chaînes de caractères noms */
+#define TAILLE_NOM 20
+
 
 
 /*
@@ -20,8 +23,7 @@
  */
 
 /*!
- * \struct	coord_s
- * \brief	Coordonnées
+ * \brief	Couple de coordonnées \c x et \c y
  */
 typedef struct coord_s {
 	int x;	/*!< Coordonnée horizontale */
@@ -29,7 +31,6 @@ typedef struct coord_s {
 } coord_t;
 
 /*!
- * \enum	etat_e
  * \brief	État des fantomes
  */
 typedef enum etat_e {
@@ -39,84 +40,104 @@ typedef enum etat_e {
 } etat_t;
 
 /*!
- * \struct	fruit_s
  * \brief	Fruit bonus
  */
 typedef struct fruit_s {
-	char * nom;			/*!< Nom du fruit */
-	int points;			/*!< Nombre de points qu'il procure */
-	coord_t * coord;	/*!< Coordonnées du fruit */
+	char nom[TAILLE_NOM];	/*!< Nom du fruit */
+	int points;				/*!< Nombre de points qu'il procure */
+	coord_t * coord;		/*!< Coordonnées du fruit */
 } fruit_t;
 
 /*!
- * \struct	joueur_s
  * \brief	Joueur
  */
 typedef struct joueur_s {
-	char * nom;			/*!< Nom du joueur */
-	int vies;			/*!< Nombre de vies */
-	int score;			/*!< Score du joueur */
-	coord_t * coord;	/*!< Coordonnée du joueur à l'instant t */
+	char nom[TAILLE_NOM];	/*!< Nom du joueur */
+	int vies;				/*!< Nombre de vies */
+	int score;				/*!< Score du joueur */
+	coord_t * coord;		/*!< Coordonnée du joueur à l'instant t */
 } joueur_t;
 
 /*!
- * \struct	fantome_s
  * \brief	Fantôme
  */
 typedef struct fantome_s {
-	char * nom;			/*!< Nom du fantôme */
-	char couleur; 		/*!< Couleur du fantôme
-						 * \li \c 'b' : bleu
-						 * \li \c 'r' : rouge
-						 * \li \c 'p' : rose
-						 * \li \c 'o' : orange
-						 */
-	coord_t (*chemin)(char [N_LAB][M_LAB], coord_t *, coord_t *); /*!<
-		* \brief	Recherche de chemin du fantôme
-		*
-		* Fonction de recherche de chemin du fantôme en fonction de son
-		* comportement.
-		*
-		*/
-	etat_t etat;		/*!< État du fantôme */
-	coord_t * coord;	/*!< Coordonnées du fantôme à l'instant t */
+	char nom[TAILLE_NOM];	/*!< Nom du fantôme */
+	/*! Couleur du fantôme 
+	 * \li \c 'b' : bleu
+	 * \li \c 'r' : rouge
+	 * \li \c 'p' : rose
+	 * \li \c 'o' : orange
+	 */
+	char couleur;
+	/*! Recherche de chemin du fantôme */
+	coord_t (*chemin)(char [N_LAB][M_LAB], coord_t *, coord_t *);
+	etat_t etat;			/*!< État du fantôme */
+	coord_t * coord;		/*!< Coordonnées du fantôme à l'instant présent */
 } fantome_t;
 
 
 
-/*
- * FONCTIONS DE CREATION DES OBJETS
- * Renvoient un pointeur sur les objets
+/*!
+ * \name	Fonctions de création des objets
+ * @{
+ * \returns		Pointeur sur l'objet créé
+ * \retval		NULL	L'objet n'a pas pu être créé
  */
 
-/* Coordonnees */
+/*!
+ * \brief		Création de coordonnées
+ * \param[in]	x, y	Coordonnées à écrire dans l'objet
+ */
 coord_t * cree_coord(int x, int y);
 
-/* Fruit bonus */
+/*!
+ * \brief		Création de fruit bonus 
+ * \param[in]	nom		Nom du fruit, ne doit pas dépasser \c TAILLE_NOM
+ * \param[in]	points	Nombre de points procurés par le bonus
+ * \param[in]	x, y	Coordonnées d'apparition du bonus
+ */
 fruit_t * cree_fruit(const char * nom, int points, int x, int y);
 
-/* Joueur */
+/*!
+ * \brief		Création du joueur
+ * \param[in]	nom		Nom du joueur, ne doit pas dépasser \c TAILLE_NOM
+ * \param[in]	x, y	Coordonnées de départ du joueur
+ */
 joueur_t * cree_joueur(const char * nom, int x, int y);
 
-/* Fantome */
+/*!
+ * \brief		Création de fantôme
+ * \param[in]	nom		Nom du fantôme
+ * \param[in]	couleur	Couleur du fantôme. Voir \ref fantome::couleur
+ * \param[in]	x, y	Coordonnées d’apparition du fantôme
+ */
 fantome_t * cree_fantome(const char * nom, char couleur, int x, int y);
 
-/*
- * FONCTIONS DE DESTRUCTION DES OBJETS
- * Renvoient un pointeur sur les objets, mis a NULL si tout se passe bien
+/*!
+ * @}
+ * \name	Fonctions de destruction des objets
+ * @{
+ * \brief			Supprime les objets et met leur adresse à \c NULL.
+ * \param[in,out]	adr		Adresse du pointeur de l'objet
+ * \retval			0		L'objet a été détruit
  */
 
 /* Coordonnees */
-coord_t * detruit_coord(coord_t ** coord);
+int detruit_coord(coord_t ** adr);
 
 /* Fruit et bonus */
-fruit_t * detruit_fruit(fruit_t ** fruit);
+int detruit_fruit(fruit_t ** adr);
 
 /* Joueur */
-joueur_t * detruit_joueur(joueur_t ** joueur);
+int detruit_joueur(joueur_t ** adr);
 
 /* Fantome */
-fantome_t * detruit_fantome(fantome_t ** fantome);
+int detruit_fantome(fantome_t ** adr);
+
+/*!
+ * @}
+ */
 
 
 
