a8270025c8b55d7c9bb55d32fb075699f529eeea
Anthony <anthony@3615yu.noho.st>
2019-04-15 15:31:17 +0200

    score.c/h deviennent osd.c/h

---
+++ gui/osd.h
@@ -0,0 +1,20 @@
+/*!
+ * \file	osd.h
+ * \author	Anthony Amiard
+ * 
+ * Affichage du score et du nombre de vies en bas de la fenêtre, et de messages
+ * au milieu du labyrinthe.
+ * 
+ */
+
+#ifndef _OSD_H_
+#define _OSD_H_
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+
+
+
+
+#endif
